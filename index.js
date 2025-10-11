70a2a219a1c0bed96e6fa0c790978d3a71e71a09
Kanomkai <thanathip.see@ku.th>
2025-10-11 16:33:02 +0700

    newpack

--- index.js
+++ index.js
@@ -87,4 +87,4 @@ app.get("/get-historical-data/:alias", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-});ห
+});
